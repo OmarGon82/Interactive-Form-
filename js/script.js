c447af0f3410731ec03e682f23123271a30298eb
Omar Gonzalez <48702844+OmarGon82@users.noreply.github.com>
2019-05-02 08:54:57 +0900

    color box

--- js/script.js
+++ js/script.js
@@ -2,10 +2,26 @@ $('#name').focus();
 $('#other-title').hide();
 
 $('#title').change(function() {
-    if($(this).val() === "other")
-    $('#other-title').show();
-    else
+    if($(this).val() === "other") {
+        $('#other-title').show();
+    }
+    else {
         $('#other-title').hide();
-    })
+    }
+    });
 
+$('#design').change(function () {
     
+if ( $(this).val() === "js puns") 
+    $('#color option[value="tomato"],[value="steelblue"],[value="dimgrey"]').hide();
+    $('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').show();
+if ($(this).val() === "heart js") 
+    $('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').hide();
+    $('#color option[value="tomato"],[value="steelblue"],[value="dimgrey"]').show();
+})
+
+// if jsPun is selected 
+// show cornflower blue, Dark slay Grey and Gold 
+// if Ijs is selected
+// show tomato, steele blue and Dim grey 
+
