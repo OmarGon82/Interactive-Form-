c6a332fadbc23c7c66ac8d1efb702a61f7f2a643
Omar Gonzalez <48702844+OmarGon82@users.noreply.github.com>
2019-05-01 13:59:05 +0900

    dropdown menu 'other' feature added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 $('#name').focus();
 $('#other-title').hide();
+
+$('#title').change(function() {
+    if($(this).val() === "other")
+    $('#other-title').show();
+    else
+        $('#other-title').hide();
+    })
+
+    
