d596484ff08a3d888a1aaf816c367f282d140e67
Omar Gonzalez <48702844+OmarGon82@users.noreply.github.com>
2019-05-02 14:27:56 +0900

    Color menu exceeds

--- js/script.js
+++ js/script.js
@@ -10,28 +10,30 @@ $('#title').change(function() {
     }
     });
 
-$('#color').children().hide();
-    
+
+$('#colors-js-puns').hide();
+const $selectTheme = $('#design option').eq(0);
 $('#design').change(function () {
+    $('#colors-js-puns').show();
+    $selectTheme.toggle();
     if ( $(this).val() === "js puns") {
         $('#color option[value="tomato"],[value="steelblue"],[value="dimgrey"]').hide();
         $('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').show();
         $('#color option[value="cornflower blue').attr('selected', true);
         $('#color option[value="tomato"]').attr('selected', false);
+        $('#color option[value="no color"]').attr('selected', false);
     } else {
         $('#color').children().hide()
+        $selectTheme.hide();
     }
     if ($(this).val() === "heart js") {
         $('#color option[value="cornflowerblue"],[value="darkslategrey"],[value="gold"]').hide();
         $('#color option[value="tomato"],[value="steelblue"],[value="dimgrey"]').show();
         $('#color option[value="tomato"]').attr('selected',true);
         $('#color option[value="cornflower blue"]').attr('selected',false);
-    } 
+        $('#color option[value="no color"]').attr('selected', false);
+    } else {$selectTheme.hide();}
     
 })
 
-// if jsPun is selected 
-// show cornflower blue, Dark slay Grey and Gold 
-// if Ijs is selected
-// show tomato, steele blue and Dim grey 
-
+const $checboxes =$("")
